c6a3790a456b4f2f84eaa762ef8f1b343aec6c35
jantonelli111 <jantonelli111@gmail.com>
2018-01-24 14:50:58 -0500

    Initial commit

---
+++ R/BayesFunctionsSigmaHetero.R
@@ -0,0 +1,225 @@
+BayesSSLemHetero = function(nScans = 20000, burn = 10000, thin = 10,
+                      p, y, x, lambda1 = 0.1,
+                      lambda0start = 20, numBlocks = 10, w,
+                      thetaA = 1, thetaB = .2*p) {
+
+  n = length(y)
+
+  betaPost = matrix(NA, nScans, p+1)
+  gammaPost = matrix(NA, nScans, p)
+  sigma2Post = rep(NA, nScans)
+  thetaPost = rep(NA, nScans)
+  tauPost = matrix(NA, nScans, p)
+  lambda0Post = rep(NA, nScans)
+
+  betaPost[1,] = rnorm(p+1, sd=0.2)
+  gammaPost[1,] = rep(0,p)
+  sigma2Post[1] = 1
+  thetaPost[1] = 0.02
+  tauPost[1,] = rgamma(p, 2)
+
+  sigmaA = 0.001
+  sigmaB = 0.001
+  lambda0 = lambda0start
+  lambda0Post[1] = lambda0
+
+  K = 10000
+
+  Design = as.matrix(cbind(rep(1, n), x))
+
+  accTheta = 0
+
+  for (i in 2 : nScans) {
+
+    if (i %% 100 == 0) print(i)
+
+    D = diag(c(K,tauPost[i-1,]))
+    Dinv = diag(1/diag(D))
+
+    ## SIGMA^2
+    SS = sum((y - Design %*% betaPost[i-1,])^2)
+    sigma2Post[i] = 1/rgamma(1, (n-1)/2 + p/2,
+                             SS/2 + t(betaPost[i-1,]) %*% Dinv %*% betaPost[i-1,]/2)
+
+    ## THETA using MH update
+    BoundaryLow2 = max(0, thetaPost[i-1] - 0.02)
+    BoundaryAbove2 = min(1, thetaPost[i-1] + 0.02)
+    thetaNew = runif(1, BoundaryLow2, BoundaryAbove2)
+
+    BoundaryLow1 = max(0, thetaNew - 0.02)
+    BoundaryAbove1 = min(1, thetaNew + 0.02)
+
+    logAR = LogTheta(thetaNew, a=thetaA, b=thetaB, w=w, gamma=gammaPost[i-1,]) +
+      dunif(thetaPost[i-1], BoundaryLow1, BoundaryAbove1, log=TRUE) -
+      LogTheta(thetaPost[i-1], a=thetaA, b=thetaB, w=w, gamma=gammaPost[i-1,]) -
+      dunif(thetaNew, BoundaryLow2, BoundaryAbove2, log=TRUE)
+
+    if (logAR > log(runif(1))) {
+      thetaPost[i] = thetaNew
+      accTheta = accTheta + 1
+    } else {
+      thetaPost[i] = thetaPost[i-1]
+    }
+
+    ## BETA
+    jumps = floor((p+1) / numBlocks)
+    betaPost[i,] = betaPost[i-1,]
+    for (jj in 1 : numBlocks) {
+      wp = ((jj-1)*jumps + 1) : (jj*jumps)
+      if (jj == numBlocks) {
+        wp = ((jj-1)*jumps + 1) : (p+1)
+      }
+
+      tempY = y - (Design[,-wp] %*% betaPost[i,-wp])
+      tempV = sigma2Post[i]*solve((t(Design[,wp]) %*% Design[,wp]) + Dinv[wp,wp])
+      tempMU = (1/sigma2Post[i])*tempV %*% t(Design[,wp]) %*% tempY
+      betaPost[i,wp] = rmvnorm(1, mean=tempMU, sigma=tempV)
+    }
+
+    ## GAMMA
+    RandOrder = sample(1:p, p, replace=FALSE)
+    for (j in RandOrder) {
+      tempProb = ((lambda1/sqrt(sigma2Post[i]))*(thetaPost[i]^w[j])*
+                    exp(-(lambda1/sqrt(sigma2Post[i]))*abs(betaPost[i,j+1]))) /
+        (((lambda1/sqrt(sigma2Post[i]))*(thetaPost[i]^w[j])*
+            exp(-(lambda1/sqrt(sigma2Post[i]))*abs(betaPost[i,j+1]))) +
+           ((lambda0Post[i-1]/sqrt(sigma2Post[i]))*(1 - thetaPost[i]^w[j])*
+              exp(-(lambda0Post[i-1]/sqrt(sigma2Post[i]))*abs(betaPost[i,j+1]))))
+      gammaPost[i,j] = rbinom(1, 1, tempProb)
+    }
+
+    ## TAU
+    RandOrder = sample(1:p, p, replace=FALSE)
+    for (j in RandOrder) {
+      tempLambda = gammaPost[i,j]*lambda1 + (1 - gammaPost[i,j])*lambda0Post[i-1]
+      lambdaPrime = tempLambda^2
+      muPrime = sqrt(tempLambda^2 * sigma2Post[i] / betaPost[i,j+1]^2)
+      tauPost[i,j] = 1 / rinvgauss(1, muPrime, lambdaPrime)
+    }
+
+    ## LAMBDA0
+    lambda0Post[i] =  lambda0
+    if (i %% 50 == 0 & i > 500) {
+      wut1 = apply(gammaPost[(i-49):i, ], 1, sum)
+      wut2 = sum(apply(tauPost[(i-49):i,] * (gammaPost[(i-49):i, ] == 0), 2, mean))
+
+      lambda0 = sqrt(2*(p - mean(wut1)) / mean(wut2))
+      diff = lambda0 - lambda0Post[i]
+      print(c(lambda0, diff))
+      lambda0Post[i] = lambda0
+
+    }
+  }
+
+  keep = seq((burn + 1), nScans, by=thin)
+  return(list(lambda0est = mean(lambda0Post[keep], na.rm=TRUE),
+              thetaEst = mean(thetaPost[keep])))
+}
+
+
+BayesSSLHetero = function(nScans, burn, thin,
+                    p, y, x, lambda1 = 0.1, lambda0,
+                    numBlocks = 10, w, thetaA = 1, thetaB = .2*p) {
+
+  n = length(y)
+
+  betaPost = matrix(NA, nScans, p+1)
+  gammaPost = matrix(NA, nScans, p)
+  sigma2Post = rep(NA, nScans)
+  thetaPost = rep(NA, nScans)
+  tauPost = matrix(NA, nScans, p)
+  lambda0Post = rep(NA, nScans)
+
+  betaPost[1,] = rnorm(p+1, sd=0.2)
+  gammaPost[1,] = rep(0,p)
+  sigma2Post[1] = 1
+  thetaPost[1] = 0.02
+  tauPost[1,] = rgamma(p, 2)
+
+  sigmaA = 0.001
+  sigmaB = 0.001
+  lambda0Post[1] = lambda0
+
+  K = 10000
+
+  Design = as.matrix(cbind(rep(1, n), x))
+
+  accTheta = 0
+
+  for (i in 2 : nScans) {
+
+    if (i %% 100 == 0) print(i)
+
+    D = diag(c(K,tauPost[i-1,]))
+    Dinv = diag(1/diag(D))
+
+    ## SIGMA^2
+    SS = sum((y - Design %*% betaPost[i-1,])^2)
+    sigma2Post[i] = 1/rgamma(1, (n-1)/2 + p/2,
+                             SS/2 + t(betaPost[i-1,]) %*% Dinv %*% betaPost[i-1,]/2)
+
+    ## THETA using MH update
+    BoundaryLow2 = max(0, thetaPost[i-1] - 0.02)
+    BoundaryAbove2 = min(1, thetaPost[i-1] + 0.02)
+    thetaNew = runif(1, BoundaryLow2, BoundaryAbove2)
+
+    BoundaryLow1 = max(0, thetaNew - 0.02)
+    BoundaryAbove1 = min(1, thetaNew + 0.02)
+
+    logAR = LogTheta(thetaNew, a=thetaA, b=thetaB, w=w, gamma=gammaPost[i-1,]) +
+      dunif(thetaPost[i-1], BoundaryLow1, BoundaryAbove1, log=TRUE) -
+      LogTheta(thetaPost[i-1], a=thetaA, b=thetaB, w=w, gamma=gammaPost[i-1,]) -
+      dunif(thetaNew, BoundaryLow2, BoundaryAbove2, log=TRUE)
+
+    if (logAR > log(runif(1))) {
+      thetaPost[i] = thetaNew
+      accTheta = accTheta + 1
+    } else {
+      thetaPost[i] = thetaPost[i-1]
+    }
+
+    ## BETA
+    jumps = floor((p+1) / numBlocks)
+    betaPost[i,] = betaPost[i-1,]
+    for (jj in 1 : numBlocks) {
+      wp = ((jj-1)*jumps + 1) : (jj*jumps)
+      if (jj == numBlocks) {
+        wp = ((jj-1)*jumps + 1) : (p+1)
+      }
+
+      tempY = y - (Design[,-wp] %*% betaPost[i,-wp])
+      tempV = sigma2Post[i]*solve((t(Design[,wp]) %*% Design[,wp]) + Dinv[wp,wp])
+      tempMU = (1/sigma2Post[i])*tempV %*% t(Design[,wp]) %*% tempY
+      betaPost[i,wp] = rmvnorm(1, mean=tempMU, sigma=tempV)
+    }
+
+    ## GAMMA
+    RandOrder = sample(1:p, p, replace=FALSE)
+    for (j in RandOrder) {
+      tempProb = ((lambda1/sqrt(sigma2Post[i]))*(thetaPost[i]^w[j])*
+                    exp(-(lambda1/sqrt(sigma2Post[i]))*abs(betaPost[i,j+1]))) /
+        (((lambda1/sqrt(sigma2Post[i]))*(thetaPost[i]^w[j])*
+            exp(-(lambda1/sqrt(sigma2Post[i]))*abs(betaPost[i,j+1]))) +
+           ((lambda0/sqrt(sigma2Post[i]))*(1 - thetaPost[i]^w[j])*
+              exp(-(lambda0/sqrt(sigma2Post[i]))*abs(betaPost[i,j+1]))))
+      gammaPost[i,j] = rbinom(1, 1, tempProb)
+    }
+
+    ## TAU
+    RandOrder = sample(1:p, p, replace=FALSE)
+    for (j in RandOrder) {
+      tempLambda = gammaPost[i,j]*lambda1 + (1 - gammaPost[i,j])*lambda0
+      lambdaPrime = tempLambda^2
+      muPrime = sqrt(tempLambda^2 * sigma2Post[i] / betaPost[i,j+1]^2)
+      tauPost[i,j] = 1 / rinvgauss(1, muPrime, lambdaPrime)
+    }
+  }
+
+  keep = seq((burn + 1), nScans, by=thin)
+  return(betaPost[keep,])
+}
+
+
+
+
+
---
+++ R/HDC.R
@@ -0,0 +1,172 @@
+#' Creating design matrix with 1d wavelet basis functions as predictors
+#'
+#' Takes in the locations at which data is observed and creates a design matrix where each column corresponds
+#' to a given wavelet basis function evaluated at the observed data locations. This function is taken from the
+#' code provided in Wand and Ormerod (2011).
+#'
+#' @param y              The outcome to be analyzed
+#' @param z              The treatment whose causal effect is to be estimated
+#' @param x              An n by p matrix of potential confounders
+#' @param lambda0        Either a numeric value to be used for the value of lambda0
+#'                       or "EM" is specified to indicate that it will be estimated
+#'                       via empirical Bayes
+#' @param lambda1        A numeric value for lambda1
+#'
+#' @return An N x K matrix containing wavelet basis functions evaluated at observed data
+#'
+#' @export
+#' @examples
+#'
+#' n <- 1000
+#' x <- runif(n)
+#'
+#' numLevels <- 3
+#' k = 2^numLevels - 1
+#' Zx <- cbind(rep(1, n), ZDaub(x, numLevels=numLevels))
+
+SSL = function(nScans = 20000, burn = 10000, thin = 10,
+               y, x, z, lambda1 = 0.1, thetaA = 1, thetaB = 0.2*dim(x)[2],
+               lambda0 = "EM") {
+
+  n = dim(x)[1]
+  p = dim(x)[2]
+
+  if (lambda0)
+
+}
+
+BayesSSLem = function(nScans = 20000, burn = 10000, thin = 10,
+                      n, p, y, x, z, lambda1 = 0.1,
+                      lambda0start = 20, numBlocks = 10, w,
+                      thetaA = 1, thetaB = .2*p) {
+
+  betaPost = matrix(NA, nScans, p+2)
+  gammaPost = matrix(NA, nScans, p)
+  sigma2Post = rep(NA, nScans)
+  thetaPost = rep(NA, nScans)
+  tauPost = matrix(NA, nScans, p)
+  lambda0Post = rep(NA, nScans)
+
+  betaPost[1,] = rnorm(p+2, sd=0.2)
+  gammaPost[1,] = rep(0,p)
+  sigma2Post[1] = 1
+  thetaPost[1] = 0.02
+  tauPost[1,] = rgamma(p, 2)
+
+  sigmaA = 0.001
+  sigmaB = 0.001
+  lambda0 = lambda0start
+  lambda0Post[1] = lambda0
+
+  K = 10000
+
+  Design = as.matrix(cbind(rep(1, n), z, x))
+
+  accTheta = 0
+
+  for (i in 2 : nScans) {
+
+    if (i %% 100 == 0) print(i)
+
+    D = diag(c(K,K,tauPost[i-1,]))
+    Dinv = diag(1/diag(D))
+
+    ## SIGMA^2
+    SS = sum((y - Design %*% betaPost[i-1,])^2)
+    sigma2Post[i] = 1/rgamma(1, (n-1)/2 + p/2,
+                             SS/2 + t(betaPost[i-1,]) %*% Dinv %*% betaPost[i-1,]/2)
+
+    ## THETA using MH update
+    BoundaryLow2 = max(0, thetaPost[i-1] - 0.02)
+    BoundaryAbove2 = min(1, thetaPost[i-1] + 0.02)
+    thetaNew = runif(1, BoundaryLow2, BoundaryAbove2)
+
+    BoundaryLow1 = max(0, thetaNew - 0.02)
+    BoundaryAbove1 = min(1, thetaNew + 0.02)
+
+    logAR = LogTheta(thetaNew, a=thetaA, b=thetaB, w=w, gamma=gammaPost[i-1,]) +
+      dunif(thetaPost[i-1], BoundaryLow1, BoundaryAbove1, log=TRUE) -
+      LogTheta(thetaPost[i-1], a=thetaA, b=thetaB, w=w, gamma=gammaPost[i-1,]) -
+      dunif(thetaNew, BoundaryLow2, BoundaryAbove2, log=TRUE)
+
+    if (logAR > log(runif(1))) {
+      thetaPost[i] = thetaNew
+      accTheta = accTheta + 1
+    } else {
+      thetaPost[i] = thetaPost[i-1]
+    }
+
+    ## BETA
+    jumps = floor((p+2) / numBlocks)
+    betaPost[i,] = betaPost[i-1,]
+    for (jj in 1 : numBlocks) {
+      wp = ((jj-1)*jumps + 1) : (jj*jumps)
+      if (jj == numBlocks) {
+        wp = ((jj-1)*jumps + 1) : (p+2)
+      }
+
+      tempY = y - (Design[,-wp] %*% betaPost[i,-wp])
+      tempV = sigma2Post[i]*solve((t(Design[,wp]) %*% Design[,wp]) + Dinv[wp,wp])
+      tempMU = (1/sigma2Post[i])*tempV %*% t(Design[,wp]) %*% tempY
+      betaPost[i,wp] = rmvnorm(1, mean=tempMU, sigma=tempV)
+    }
+
+    ## GAMMA
+    RandOrder = sample(1:p, p, replace=FALSE)
+    for (j in RandOrder) {
+      tempProb = ((lambda1/sqrt(sigma2Post[i]))*(thetaPost[i]^w[j])*
+                    exp(-(lambda1/sqrt(sigma2Post[i]))*abs(betaPost[i,j+2]))) /
+        (((lambda1/sqrt(sigma2Post[i]))*(thetaPost[i]^w[j])*
+            exp(-(lambda1/sqrt(sigma2Post[i]))*abs(betaPost[i,j+2]))) +
+           ((lambda0Post[i-1]/sqrt(sigma2Post[i]))*(1 - thetaPost[i]^w[j])*
+              exp(-(lambda0Post[i-1]/sqrt(sigma2Post[i]))*abs(betaPost[i,j+2]))))
+      gammaPost[i,j] = rbinom(1, 1, tempProb)
+    }
+
+    ## TAU
+    RandOrder = sample(1:p, p, replace=FALSE)
+    for (j in RandOrder) {
+      tempLambda = gammaPost[i,j]*lambda1 + (1 - gammaPost[i,j])*lambda0Post[i-1]
+      lambdaPrime = tempLambda^2
+      muPrime = sqrt(tempLambda^2 * sigma2Post[i] / betaPost[i,j+2]^2)
+      tauPost[i,j] = 1 / rinvgauss(1, muPrime, lambdaPrime)
+    }
+
+    ## LAMBDA0
+    lambda0Post[i] =  lambda0
+    if (i %% 50 == 0 & i > 500) {
+      wut1 = apply(gammaPost[(i-49):i, ], 1, sum)
+      wut2 = sum(apply(tauPost[(i-49):i,] * (gammaPost[(i-49):i, ] == 0), 2, mean))
+
+      lambda0 = sqrt(2*(p - mean(wut1)) / mean(wut2))
+      diff = lambda0 - lambda0Post[i]
+      print(c(lambda0, diff))
+      lambda0Post[i] = lambda0
+
+    }
+  }
+
+  keep = seq((burn + 1), nScans, by=thin)
+  return(list(lambda0est = mean(lambda0Post[keep], na.rm=TRUE),
+              thetaEst = mean(thetaPost[keep])))
+}
+
+
+
+expit = function(x) {
+  return(exp(x) / (1 + exp(x)))
+}
+
+pStar = function(beta, theta, lambda1, lambda0) {
+  part1 = theta*lambda1*exp(-lambda1*abs(beta))
+  part0 = (1-theta)*lambda0*exp(-lambda0*abs(beta))
+  return(part1 / (part1 + part0))
+}
+
+LogTheta = function(theta, a, b, w, gamma) {
+  part1 = (a-1)*log(theta)
+  part2 = (b-1)*log(1 - theta)
+  part3 = sum(w*gamma)*log(theta)
+  part4 = sum(log((1 - theta^w)^(1-gamma)))
+  return(part1 + part2 + part3 + part4)
+}
